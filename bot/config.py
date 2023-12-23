6db2e27d83384822fb3c9f59d2d9bd1ff725edfe
Animepahe498 <148706595+Animepahe498@users.noreply.github.com>
2023-12-23 17:10:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config(object):
      # Get these values from my.telegram.org
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "AUTH_USERS", "5086525318,6446763201,961551026,760067286,6066177103"
+            "AUTH_USERS",{5086525318,6446763201,961551026,760067286,6066177103}
             should_prompt=True
         ).split()
     )
